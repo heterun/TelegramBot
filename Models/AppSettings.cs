9fdc8f95e4784514aa3b6ebae5bf060c4a1543d4
heterun <39742077+heterun@users.noreply.github.com>
2019-10-26 19:17:32 -0700

    Summary

---
+++ Models/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelegramBot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://telegrambotapp.azurewebsites.net:443/{0}";
+
+        //name bot
+        public static string Name { get; set; } = "first bot app telegram";
+
+        //api
+        public static string Key { get; set; } = "912179562:AAGak6ITHXCxMkdFgJyO1QpZJDB7Oo-IhAw";
+    }
+}
