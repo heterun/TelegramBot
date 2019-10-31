870ab4c1e323455145923954669e6544dcc9af6b
heterun <39742077+heterun@users.noreply.github.com>
2019-10-31 15:59:10 -0700

    MessageController
    add message controller
    modify home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,23 +8,10 @@ namespace TelegramBot.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+       public string Index()
         {
-            return View();
+            return "It's my telega bot)";
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
