fabfd7db0b8c771f46cf7da0c4d7cd4866ac03ce
heterun <39742077+heterun@users.noreply.github.com>
2019-10-31 16:02:30 -0700

    Update MessageController.cs

--- Controllers/MessageController.cs
+++ Controllers/MessageController.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Results;
+using Telegram.Bot.Types;
+using TelegramBot.Models;
 
 namespace TelegramBot.Controllers
 {
     public class MessageController : ApiController
     {
+        [Route(@"api/message/update")] // webhook uri part
+        public async Task<OkResult> Update([FromBody]Update update) // relevant info for bot
+        {
+            var commands = Bot.Commands;
+            var message = update.Message; // message for bot
+            var client = await Bot.Get(); // telegram bot client in order to send an answer // get is async
+
+            foreach(var command in commands)
+            {
+                if (command.Contains(message.Text))
+                {
+                    command.Execute(message, client);
+                    break;
+                }
+            }
+
+            return Ok();
+        }
     }
 }
